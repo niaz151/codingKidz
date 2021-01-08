fb42b74058257cf50bd055f602aa130efc6f88ea
niaz151 <niaz151@gmail.com>
2021-01-08 11:03:22 -0500

    remove unneccesary container

--- src/App.tsx
+++ src/App.tsx
@@ -30,56 +30,48 @@ const App: React.FC = () => {
 
   return (
     <div className="app-container">
-      <Container>
-        <Col>
-          <Row>
-            <Navbar fixed="bottom">
-              <Navbar.Brand>
-                <Link to="/">codingKIDZ Quizapp</Link>
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse>
-                <Nav className="mr-auto">
-                  <Nav.Item>
-                    <Nav.Link as={Link} to="/units">
-                      Units
-                    </Nav.Link>
-                  </Nav.Item>
-                  {user ? (
-                    <Button onClick={handleSignOut}>Logout</Button>
-                  ) : (
-                    <Nav.Item>
-                      <Nav.Link as={Link} to="/login">
-                        Login
-                      </Nav.Link>
-                    </Nav.Item>
-                  )}
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </Row>
-          <Row>
-            <Switch>
-              <PublicRoute exact path="/login" component={Login} />
-              <PublicRoute exact path="/register" component={Register} />
-              <PublicRoute exact path="/" component={Splash} />
-              <PrivateRoute exact path="/home" component={Home} />
-              <Route exact path="/passwordreset" component={PasswordReset} />
-              <PrivateRoute exact path="/units" component={Units} />
-              <PrivateRoute
-                exact
-                path="/units/edit/:unit_id/:topic_id"
-                component={EditTopic}
-              />
-              <PrivateRoute
-                exact
-                path="/units/quiz/:unit_id/:topic_id"
-                component={Quiz}
-              />
-            </Switch>
-          </Row>
-        </Col>
-      </Container>
+      <Navbar fixed="bottom">
+        <Navbar.Brand>
+          <Link to="/">codingKIDZ Quizapp</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/units">
+                Units
+              </Nav.Link>
+            </Nav.Item>
+            {user ? (
+              <Button onClick={handleSignOut}>Logout</Button>
+            ) : (
+              <Nav.Item>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+              </Nav.Item>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <PublicRoute exact path="/login" component={Login} />
+        <PublicRoute exact path="/register" component={Register} />
+        <PublicRoute exact path="/" component={Splash} />
+        <PrivateRoute exact path="/home" component={Home} />
+        <Route exact path="/passwordreset" component={PasswordReset} />
+        <PrivateRoute exact path="/units" component={Units} />
+        <PrivateRoute
+          exact
+          path="/units/edit/:unit_id/:topic_id"
+          component={EditTopic}
+        />
+        <PrivateRoute
+          exact
+          path="/units/quiz/:unit_id/:topic_id"
+          component={Quiz}
+        />
+      </Switch>
     </div>
   );
 };
