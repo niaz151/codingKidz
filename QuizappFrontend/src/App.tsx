a8644da98201c6921fdc1dc789f5dbc72a78c20a
niaz151 <niaz151@gmail.com>
2021-02-05 02:15:21 -0500

    Created Splash With Styles

--- QuizappFrontend/src/App.tsx
+++ QuizappFrontend/src/App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
-import {StatusBar} from 'react-native';
-import React from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
 import TestPage from './pages/TestPage';
 import Splash from './pages/Splash';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -8,53 +8,91 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {UnitsStack, HomeStack} from './pages';
 import {SafeAreaView} from 'react-native-safe-area-context';
-
 const Tab = createBottomTabNavigator();
 
-const App = () => {
-  return (
-    <SafeAreaView>
-      <NavigationContainer>
-        <StatusBar barStyle="dark-content" />
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              switch (route.name) {
-                case 'Home':
-                  return (
-                    <Ionicons
-                      name={`home-${focused ? 'outline' : 'sharp'}`}
-                      size={size}
-                      color={color}
-                    />
-                  );
-                case 'Units':
-                  return (
-                    <Ionicons
-                      name={`book-${focused ? 'outline' : 'sharp'}`}
-                      size={size}
-                      color={color}
-                    />
-                  );
-                case 'Settings':
-                  return (
-                    <Ionicons name={`cog-${focused ? 'outline' : 'sharp'}`} />
-                  );
-              }
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-          }}>
-          <Tab.Screen name="Splash" component={Splash} />
-          <Tab.Screen name="Home" component={HomeStack} />
-          <Tab.Screen name="Units" component={UnitsStack} />
-          <Tab.Screen name="Settings" component={TestPage} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-  );
+
+interface CustomProps {
+  showSplash: boolean
+}
+
+interface CustomState {
+  showSplash: boolean
+}
+
+class App extends Component<CustomProps, CustomState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showSplash: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({showSplash:false})
+    }, 2500)
+  }
+
+  render(){
+    if(this.state.showSplash){
+      return(
+        <SafeAreaView style={styles.viewStyles}>
+          <Splash/>    
+        </SafeAreaView>
+      )
+    }
+    else{
+      return (
+        <SafeAreaView>
+          <NavigationContainer>
+            <StatusBar barStyle="dark-content" />
+            <Tab.Navigator
+              screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                  switch (route.name) {
+                    case 'Home':
+                      return (
+                        <Ionicons
+                          name={`home-${focused ? 'outline' : 'sharp'}`}
+                          size={size}
+                          color={color}
+                        />
+                      );
+                    case 'Units':
+                      return (
+                        <Ionicons
+                          name={`book-${focused ? 'outline' : 'sharp'}`}
+                          size={size}
+                          color={color}
+                        />
+                      );
+                    case 'Settings':
+                      return (
+                        <Ionicons name={`cog-${focused ? 'outline' : 'sharp'}`} />
+                      );
+                  }
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}>
+              <Tab.Screen name="Home" component={HomeStack} />
+              <Tab.Screen name="Units" component={UnitsStack} />
+              <Tab.Screen name="Settings" component={TestPage} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      );
+    }
+  }
 };
+
+const styles = StyleSheet.create({
+  viewStyles:{
+    backgroundColor:"#FF671D"
+  }
+}) 
 
 export default App;
--- QuizappFrontend/src/pages/Splash.tsx
+++ QuizappFrontend/src/pages/Splash.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { View, Text, StyleSheet, Image} from "react-native";
 import splash_logo from '../assets/images/splash_logo.svg';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-const Splash = (props: { auth: boolean }) => {
-  const { auth } = props;
+
+const Splash = () => {
   return (
     <View style={styles.splashContainer}>
       <View style={styles.splashLogoTitleWrap}>
@@ -17,86 +18,78 @@ const Splash = (props: { auth: boolean }) => {
         </View>
       </View>
       <View style={styles.splashCaptionWrap}>
-        <View>
-          <Text> lorem ipsum dolor sit amet, cosnectetur adipiscing elit. </Text>
-        </View>
+        <Text> lorem ipsum dolor sit amet, cosnectetur adipiscing elit. </Text>
       </View>
-      {auth && (
-        <View style={styles.splashBtnWrap}>
-          <View style={styles.splashBtnOne}>
-            <Text>LOG IN</Text>
-          </View>
-          <View style={styles.splashBtnTwo}>
-            <Text> SIGN UP </Text> 
-          </View>
-        </View>
-      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   splashContainer:{
-    height: "100vh",
-    width: "100vw",
+    height: hp('100%'),
+    width: wp('100%'),
     display: "flex",
     alignItems:"center",
     justifyContent: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   splashLogoTitleWrap:{
-    height: "80px",
-    width: "360px",
+    height: 80,
+    width: 360,
     display: "flex",
+    flexDirection:"row",
     alignItems:"center",
     justifyContent:"center",
     color:"white",
     position:"absolute",
-    top:0
+    top:100
   },
   splashLogoTitleOne:{
     fontWeight:"bold",
-    fontSize: 50,
+    fontSize: 30,
   },
   splashLogoTitleTwo:{
-    fontSize: 45,
+    fontSize: 30,
+    marginLeft:-10
   },
   splashLogoImgWrap:{
-    height: "270px",
-    width: "270px",
+    height: 270,
+    width: 270,
     display:"flex",
     alignItems: "center",
     justifyContent:"center",
-    marginTop:"3rem"
+    marginTop:3,
   },
   splashLogoCirclePortrait:{
-    borderRadius: 50,
-    width: "270px",
-    height: "270px",
+    borderRadius: 200,
+    width: 270,
+    height: 270,
     backgroundColor: "#FCFADD",
     borderWidth: 5,
     borderColor: "#F8EB30",
-    marginTop:"3rem"
+    marginTop:3
   },
   splashCaptionWrap:{
     color:"white",
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: "bold",
-    width: "250px",
-    textAlign: "center",
-    marginTop:"3rem"
+    width: 250,
+    marginTop:50,
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center"
   },
   splashBtnWrap:{
     display:"flex",
     alignItems:"center",
     justifyContent:"space-around",
-    height: "70px",
-    width: "300px",
-    marginTop:"3rem"
+    height: 70,
+    width: 300,
+    marginTop:3
   },
   splashBtn:{
-    height: "50px",
-    width: "120px",
+    height: 50,
+    width: 120,
     display:"flex",
     alignItems:"center",
     justifyContent: "center",
