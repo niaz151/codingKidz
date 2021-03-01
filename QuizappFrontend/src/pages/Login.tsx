c433c72dec91d51529768205780c73814b949951
SammyG <geller.sammy@gmail.com>
2021-02-28 21:28:50 -0500

    add login connection/demo

--- QuizappFrontend/src/pages/Login.tsx
+++ QuizappFrontend/src/pages/Login.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
-import { View, Text, StyleSheet,TouchableOpacity} from "react-native";
-import { TextInput, Button } from 'react-native-paper';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import axios from 'axios';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Alert} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [accessToken, setAccessToken] = useState<string>();
+  const [refreshToken, setRefreshToken] = useState<string>();
 
-  const handleChange = (txt) => {
-    console.log(txt);
-  }
+  const handleSubmit = async () => {
+    await axios
+      .post('http://localhost:8000/api/auth/login', {
+        email: email,
+        password: password,
+      })
+      .then(
+        (response) => {
+          setAccessToken(response.data.access_token);
+          setRefreshToken(response.data.refresh_token);
+        },
+        (error) => {
+          console.log('error occured', error);
+          Alert.alert('Error!');
+        },
+      );
+  };
 
   return (
     <View style={styles.loginContainer}>
+      <Text>
+        Access Token: {accessToken ? accessToken : 'Sign in to set me'}
+      </Text>
+      <Text>
+        Refresh Token: {refreshToken ? refreshToken : 'Sign in to set me'}
+      </Text>
       <View style={styles.inputContainer}>
-        <TextInput label="Email" onChangeText={text => handleChange(text)} style={styles.textInput} />
-        <TextInput label="Password" onChangeText={text => handleChange(text)} style={styles.textInput} />
-        <Button mode="contained" style={styles.btn}> SIGN IN </Button>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.textInput}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.textInput}
+        />
+        <Button mode="contained" style={styles.btn} onPress={handleSubmit}>
+          SIGN IN
+        </Button>
         <Text style={styles.forgot}> Forgot Password? </Text>
       </View>
       <Text style={styles.privacy}> Privacy Policy </Text>
@@ -23,48 +63,48 @@ const LoginPage = () => {
 };
 
 const styles = StyleSheet.create({
-  loginContainer:{
-    height: hp("100%"),
-    width: wp("100%"),
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    position: 'relative'
+  loginContainer: {
+    height: hp('100%'),
+    width: wp('100%'),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
   },
-  inputContainer:{
-    height: hp("30%"),
-    width: wp("100%"),
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-around"
+  inputContainer: {
+    height: hp('30%'),
+    width: wp('100%'),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
-  textInput:{
-    width: wp("80%"),
+  textInput: {
+    width: wp('80%'),
     height: 50,
-    borderWidth:1,
+    borderWidth: 1,
     borderRadius: 10,
-    borderColor:'#FF671D'
+    borderColor: '#FF671D',
   },
-  btn:{
+  btn: {
     height: 50,
     width: 150,
     backgroundColor: '#FF671D',
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 10,
     fontSize: 15,
   },
-  forgot:{
+  forgot: {
     color: '#FF671D',
-    fontSize: 15
+    fontSize: 15,
   },
-  privacy:{
+  privacy: {
     position: 'absolute',
-    bottom: hp("10%"),
+    bottom: hp('10%'),
     color: '#1B6A7A',
-    fontSize: 14
-  }
-})
+    fontSize: 14,
+  },
+});
 
 export default LoginPage;
