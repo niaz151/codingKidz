7cf87c06b4b7cd962e6f4b545408c2052f17a4cd
SammyG <geller.sammy@gmail.com>
2021-03-14 13:46:50 -0400

    update unit fetching/displaying logic

--- QuizappFrontend/src/pages/Units/UnitsPage.tsx
+++ QuizappFrontend/src/pages/Units/UnitsPage.tsx
@@ -1,53 +1,69 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp,} from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 import {Text, View} from 'react-native';
 import axios from 'axios';
 import {Button} from 'react-native-paper';
 
-type Unit = {
-  id: number, 
-  name: string, 
-  number: number}
+interface Unit {
+  id: number;
+  name: string;
+  number: number;
+}
 
-export const UnitsPage = (props: {accessToken: string;logout: () => Promise<void>;}) => {
-
+export const UnitsPage = (props: {
+  accessToken: string;
+  logout: () => Promise<void>;
+}) => {
   const navigation = useNavigation();
   const {accessToken, logout} = props;
   const [units, setUnits] = useState<Unit[]>();
 
   useEffect(() => {
-    getUnits();
-  });
+    const getUnits = async () => {
+      return await axios
+        .get('http://localhost:8000/api/unit', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then(
+          (response) => {
+            console.log('RESPONSE DATA', response.data);
+            let tempUnits: Unit[] = [];
+            response.data.units.map((unit: Unit) => {
+              tempUnits.push(unit);
+            });
+            setUnits(tempUnits);
+          },
+          (error) => {
+            console.log('fetching error', error);
+          },
+        );
+    };
 
-  const getUnits = async () => {
-    await axios
-    .get('http://localhost:8000/api/unit', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-    .then(
-      (response) => {
-        setUnits(response.data.units);
-        console.log("units:",units)
-      },
-      (error) => {
-        console.log('fetching error', error);
-      },
-    ); 
-  };
-  
+    getUnits();
+  }, [accessToken]);
+
   return (
     <View style={styles.container}>
       <View style={styles.title}>
         <Text style={styles.titleText}> LET'S LEARN SCRATCH! </Text>
       </View>
-      <View style={styles.unitsList}>
-      {units && units.map( (unit) => {
-
-      })}
-      </View>
+      {units ? (
+        <View style={styles.unitsList}>
+          {units.map((unit) => {
+            <Text>Unit: {unit.name}</Text>;
+          })}
+        </View>
+      ) : (
+        <View>
+          <Text>Loading units...</Text>
+        </View>
+      )}
       <Button onPress={logout}>Logout</Button>
     </View>
   );
@@ -74,26 +90,26 @@ const styles = {
   unitsList: {
     height: hp('70%'),
     width: wp('100%'),
-    flex:1,
-    flexWrap:'wrap',
-    flexDirection:'row',
-    display:'flex',
-    justifyContent:'space-around',
-    alignItems: 'center'
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   unitTileContainer: {
     borderWidth: 1.5,
     borderColor: 'white',
-    borderRadius:5,
+    borderRadius: 5,
     height: hp('10%'),
     maxHeight: 70,
     width: wp('35%'),
     maxWidth: 200,
-    flexBasis:"40%",
-    marginTop:hp("10%"),
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
+    flexBasis: '40%',
+    marginTop: hp('10%'),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   unitTileText: {},
 };
