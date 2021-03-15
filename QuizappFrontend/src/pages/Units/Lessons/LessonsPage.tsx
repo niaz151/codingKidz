e62398bba3376a6bbdb1446296890a4cbe8550ba
niaz151 <niaz151@gmail.com>
2021-03-14 23:45:49 -0400

    fetch topics

--- QuizappFrontend/src/pages/Units/Lessons/LessonsPage.tsx
+++ QuizappFrontend/src/pages/Units/Lessons/LessonsPage.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native'
 import { useRoute } from '@react-navigation/native';
-
+import axios from 'axios';
+import {Topic} from '../../../utils';
 
 export const LessonsPage = (props: {
   accessToken: string;
@@ -9,6 +10,34 @@ export const LessonsPage = (props: {
 
   const route:any = useRoute();
   const id = route.params.id;
+  const accessToken = props.accessToken;
+  const [topics, setTopics] = useState<Topic[]>();
+
+  useEffect(() => {
+    const getTopics = async () => {
+      return await axios
+        .get(`http://localhost:8000/api/unit/${id}/topic/`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then(
+          (response) => {
+            console.log('RESPONSE DATA', response.data);
+            let tempTopics: Topic[] = [];
+             response.data.topics.map((topic: Topic) => {
+              tempTopics.push(topic);
+            });
+            setTopics(tempTopics);
+          },
+          (error) => {
+            console.log('fetching error', error);
+          },
+        );
+    };
+
+    getTopics();
+  }, [accessToken]);
 
   return(
     <View>
