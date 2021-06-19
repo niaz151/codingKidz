d40c3dda1aa7287716cdcbebf2bccaeee9e1b9d1
niaz151 <niaz151>
2021-06-19 16:24:37 -0400

    add true/false button

--- QuizappFrontend/src/pages/Units/Quiz/components/TrueFalseQuestionContainer.tsx
+++ QuizappFrontend/src/pages/Units/Quiz/components/TrueFalseQuestionContainer.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Button, Text} from 'react-native-paper';
 import {TrueFalseQuestion, shuffleArray} from '../../../../utils';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
 
 type Props = {
   question: TrueFalseQuestion;
@@ -29,12 +34,59 @@ const TrueFalseQuestionContainer = (props: Props) => {
   };
 
   return (
-    <View>
-      <Text>{question.question}</Text>
-      <Button onPress={selectedTrue}>True</Button>
-      <Button onPress={selectedFalse}>False</Button>
+    <View style={styles.container}>
+      <View style={styles.questionContainer}>
+        <Text>{question.question}</Text>
+      </View>
+      <View style={styles.optionsContainer}>
+        <Button style={styles.optionButton}>
+          <Text style={styles.optionText}> True </Text>
+        </Button>
+        <Button style={styles.optionButton}>
+          <Text style={styles.optionText}> False </Text>
+        </Button>
+      </View>
+      {/*
+        <Text>{question.question}</Text>
+        <Button onPress={selectedTrue}>True</Button>
+        <Button onPress={selectedFalse}>False</Button>
+      */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    height: hp("100%"),
+    width:wp("100%")
+  },
+  questionContainer: {
+    height: hp('30%'),
+    width: wp('100%'),
+  },
+  optionsContainer:{
+    height: hp("25%"),
+    width: wp("100%"),
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'space-around'
+  },
+  optionButton:{
+    height: hp("7%"),
+    width: wp("60%"),
+    borderRadius: 20,
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#FF6A00',
+    paddingTop:10
+  },
+  optionText:{
+    fontFamily:'Nexa Bold',
+    fontSize: 30,
+    color:'white'
+  }
+})
 
 export default TrueFalseQuestionContainer;
