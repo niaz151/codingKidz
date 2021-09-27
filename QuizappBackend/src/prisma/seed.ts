5c65f8f0771e3ef28376b57abead69a1cdaa4a27
niaz151 <niaz151>
2021-09-26 22:34:36 -0400

    question creation maps over hardcoded array

--- QuizappBackend/src/prisma/seed.ts
+++ QuizappBackend/src/prisma/seed.ts
@@ -3,109 +3,80 @@ import bcrypt from "bcrypt";
 import { Console } from "console";
 
 const prisma = new PrismaClient();
-
-var questions = [
+var mc_questions = [
   {
+    id: 0,
     type: "MC",
-    correct: "option_1",
-    text: "After the green flag was clicked, the Sprite moved. Which Event block was used?",
-    questions: {
-      option_1: "When Backdrop Switches",
-      option_2: "When Green Flag Clicked",
-      option_3: "Move 10 Steps",
-      option_4: "When this Sprite Clicked",
-    },
+    correctAnswer: "When this Sprite Clicked",
+    question: "After the green flag was clicked, the Sprite moved. Which Event block was used?",
+    wrongAnswer0: "When Backdrop Switches",
+    wrongAnswer1: "When Green Flag Clicked",
+    wrongAnswer2: "Move 10 Steps",
   },
   {
+    id: 1,
     type: "MC",
-    correct: "option_2",
-    text: "When the backdrop switched, the Sprite began to spin. When Event block was used?",
-    questions: {
-      option_1: "When this Sprite clicked",
-      option_2: "When Backdrop switches",
-      option_3: "When Green Flag Clicked",
-      option_4: "When Space Key Pressed",
-    },
+    correctAnswer: "When this Sprite Clicked",
+    question: "When the backdrop switched, the Sprite began to spin. Which Event block was used?",
+    wrongAnswer0: "When Green Flag Clicked",
+    wrongAnswer1: "When Backdrop Switches",
+    wrongAnswer2: "When Space Key Pressed",
   },
   {
+    id: 2, 
     type: "MC",
-    correct: "option_3",
-    text: "When I pessed the Up Arrow Key, the Sprite moved up. Which Event block was used?",
-    questions: {
-      option_1: "When Green Flag Clicked",
-      option_2: "When Space Key Pressed",
-      option_3: "When Green Flash Clicked",
-      option_4: "When Space Key Pressed",
-    },
+    correctAnswer: "When this Sprite Clicked",
+    question: "When I pressed the Up Arrow Key, the Sprite moved up. Which Event block was used",
+    wrongAnswer0: "When Space Key Pressed",
+    wrongAnswer1: "When Up Arrow Key Pressed",
+    wrongAnswer2: "When Green Flag Clicked",
   },
   {
+    id: 3, 
     type: "MC",
-    correct: "option_4",
-    text: "When I pressed the Green Flag, the Sprite began to move. When I clicked on the Sprite, the Sprite began to spin. When Event block was uesd to make the Sprite spin?",
-    questions: {
-      option_1: "When Green FLag Clicked",
-      option_2: "When Space Key Pressed",
-      option_3: "When Up Arrow Key Pressed",
-      option_4: "When this Sprite Clicked",
-    },
+    correctAnswer: "When this Sprite Clicked",
+    question: "When I pressed the Green Flag, the Sprite began to move. When I clicked on the Sprite, the Sprite began to spin. Which Event block was used to make the Sprite spin?",
+    wrongAnswer0: "When Backdrop Switches",
+    wrongAnswer1: "When Green Flag Clicked",
+    wrongAnswer2: "When Space Key Pressed",
   },
   {
+    id: 4, 
     type: "MC",
-    correct: "option_1",
-    text: "When I pressed the Green Flag, the Sprite disappeared. When I pressed the Space Key, the Sprite reappeared. Which two Event blocks were used to make the sprite disappear and reappear?",
-    questions: {
-      option_1: "When Green Flag Clicked and when Backdrop Switches",
-      option_2: "When Space Key Pressed and when this Sprite Clicked",
-      option_3: "When Green Flag Clicked and when Space Key Pressed",
-      option_4: "When Backdrop Switches and when this Sprite Clicked",
-    },
-  },
-  {
-    type: "TF",
-    correct: "option_2",
-    text: "When I clicked the Green Flag, the Sprite moved. That means I used the 'When Space Key Pressed' block.",
-    questions: {
-      option_1: "False",
-      option_2: "True",
-    },
-  },
-  {
-    type: "TF",
-    correct: "option_2",
-    text: "When the Backdrop Switched, the Sprite changed colors. That means I used the 'When Backdrop Switches' block.",
-    questions: {
-      option_1: "False",
-      option_2: "True",
-    },
-  },
-  {
-    type: "TF",
-    correct: "option_1",
-    text: "When the Green Flag was clicked, the Sprite started to change colors. When I clicked on the Sprite, the Sprite began to spin. That means I used the 'When This Sprite Clicked' block to make the Sprite spin.",
-    questions: {
-      option_1: "False",
-      option_2: "True",
-    },
-  },
-  {
-    type: "TF",
-    correct: "option_1",
-    text: "When I pressed the space key, the Sprite moved. That means I used the 'When Green Flag Clicked' block.",
-    questions: {
-      option_1: "False",
-      option_2: "True",
-    },
-  },
-  {
-    type: "TF",
-    correct: "option_2",
-    text: "When I clicked the Green Flag, the Sprite began to spin around.",
-    questions: {
-      option_1: "False",
-      option_2: "True",
-    },
+    correctAnswer: "When this Sprite Clicked",
+    question: "When I pressed the Green Flag, the Sprite disappeared. When I pressed the Space Key, the Sprite reappeared. Which two Event blocks were used to make the sprite disappear and reappear? ",
+    wrongAnswer0: "When Green Flag Clicked and When Backdrop Switches",
+    wrongAnswer1: "When Green Flag Clicked and When Space Key Pressed",
+    wrongAnswer2: "When Backdrop Switches and When this Sprite Clicked",
   },
 ];
+var tf_questions = [
+  {
+    id:5,
+    question: "When I clicked the Green Flag, the Sprite moved. That means I used the ‘When Space Key Pressed’ block.",
+    correctAnswer: true,
+  },
+  {
+    id:6,
+    question: "When the Backdrop Switched, the Sprite changed colors. That means I used the ‘When Backdrop Switches’ block.",
+    correctAnswer: true,
+  },
+  {
+    id:7,
+    question: "When the Green Flag was clicked, the Sprite started to change colors. When I clicked on the Sprite, the Sprite began to spin. That means I used the ‘When this Sprite Clicked’ block to make the Sprite spin.",
+    correctAnswer: true,
+  },
+  {
+    id:8,
+    question: "When I pressed the space key, the Sprite moved. That means I used the ‘When Green Flag Clicked’ block.",
+    correctAnswer: true,
+  },
+  {
+    id:9,
+    question: "When I pressed the green flag, the Sprite began to spin around. That means I used the ‘When Green Flag Clicked’ block.",
+    correctAnswer: true,
+  }
+]
 
 export const seed = async () => {
   try {
@@ -170,23 +141,22 @@ const generateData = async () => {
                   name: `Lesson ${topicval}`,
                   number: topicval,
                   multipleChoiceQuestions: {
-                    create: [...Array(40).keys()].map((questionval) => {
+                    create: mc_questions.map((question) => {
                       return {
-                        // changing it to questions[questionval] throws error because this maps over 40 
-                        // whereas I have 10 samples questions
-                        question: `Question ${questions[0].text}`,
-                        correctAnswer: `Correct ${questions[0].questions.option_1}`,
-                        wrongAnswer0: `Wrong 0 ${questions[0].questions.option_2}`,
-                        wrongAnswer1: `Wrong 1 ${questions[0].questions.option_3}`,
-                        wrongAnswer2: `Wrong 2 ${questions[0].questions.option_4}`,
+                        // try to have the same number of tf and mc questions
+                        question: `${question.question}`,
+                        correctAnswer: `${question.correctAnswer}`,
+                        wrongAnswer0: `${question.wrongAnswer0}`,
+                        wrongAnswer1: `${question.wrongAnswer1}`,
+                        wrongAnswer2: `${question.wrongAnswer2}`,
                       };
                     }),
                   },
                   trueFalseQuestions: {
-                    create: [...Array(40).keys()].map((questionval) => {
+                    create: tf_questions.map((question) => {
                       return {
-                        question: `Question ${questionval}`,
-                        correctAnswer: questionval % 2 === 0,
+                        question: `${question.question}`,
+                        correctAnswer: question.correctAnswer,
                       };
                     }),
                   },
